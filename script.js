048a800bfe19c53bc192538d37cebdd8a3f6d6e5
David Oliveira <daviddenisson2014@gmail.com>
2022-12-30 14:33:32 -0300

    Ajustes responsividade

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+window.addEventListener('scroll', () =>{
+    let navbar = document.querySelector('.navbar');
+
+    let windowPosition = window.scrollY > 0;
+
+    navbar.classList.toggle('scroll', windowPosition);
+})
+
+
+const startInteractive = () => {
+    let getButtonNavbar = document.querySelectorAll('.navbar-link');
+    getButtonNavbar.forEach(button => button.addEventListener('click', () => {NavbarConfig.changeButton(button)}))
+}
+
+const NavbarConfig = {
+    removeActive(){
+        let activeButton = document.querySelector('.navbar-link.active');
+        activeButton.classList.remove('active');
+        return;
+    },
+    changeButton(button){
+        
+        this.removeActive();
+        let buttonToActive = button;
+        buttonToActive.classList.add('active');
+
+    }
+}
+
+
+
+
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         autoHeight: false,
@@ -14,11 +46,13 @@ $(document).ready(function () {
                 items: 3
             }
         },
-       
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true
+
     });
 })
 
 
-// autoplay: true,
-// autoplayTimeout: 2000,
-// autoplayHoverPause: true
+startInteractive();
+
