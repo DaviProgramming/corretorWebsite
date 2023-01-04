91be283a954faf97413a0eca80821c35684faa77
David Oliveira <queriaprogramar@gmail.com>
2023-01-03 22:06:38 -0300

    Ajuste interatividade javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', () =>{
+window.addEventListener('scroll', () => {
     let navbar = document.querySelector('.navbar');
 
     let windowPosition = window.scrollY > 0;
@@ -6,42 +6,97 @@ window.addEventListener('scroll', () =>{
     navbar.classList.toggle('scroll', windowPosition);
 })
 
+window.addEventListener('load', () => {
+    containerConfig.getContainer();
+})
 
 const startInteractive = () => {
     let getButtonNavbar = document.querySelectorAll('.navbar-link');
-    getButtonNavbar.forEach(button => button.addEventListener('click', () => {NavbarConfig.changeButton(button)}))
+    getButtonNavbar.forEach(button => button.addEventListener('click', () => { NavbarConfig.changeButton(button) }))
 
     let hamburguerButton = document.querySelector('.navbar-hamburguer');
-    hamburguerButton.addEventListener('click', ()=> {NavbarConfig.mobileOpen()})
+    hamburguerButton.addEventListener('click', () => { NavbarConfig.mobileOpen() })
 
     let closeButton = document.querySelector('.navbar-close');
-    closeButton.addEventListener('click', () =>{NavbarConfig.mobileClose()})
+    closeButton.addEventListener('click', () => { NavbarConfig.mobileClose() })
 
 }
 
 const NavbarConfig = {
-    removeActive(){
+    removeActive() {
         let activeButton = document.querySelector('.navbar-link.active');
-        activeButton.classList.remove('active');
+
+        if (activeButton != null) {
+            activeButton.classList.remove('active');
+        }
         return;
     },
-    changeButton(button){
-        
+    changeButton(button) {
+
         this.removeActive();
         let buttonToActive = button;
         buttonToActive.classList.add('active');
 
     },
-    mobileOpen(){
+    mobileOpen() {
         let navbar = document.querySelector('.navbar');
         navbar.classList.add('show');
     },
-    mobileClose(){
+    mobileClose() {
         let navbar = document.querySelector('.navbar');
         navbar.classList.remove('show');
     }
 }
 
+const containerConfig = {
+    getContainer() {
+        let containers = document.querySelectorAll('.container');
+        containers.forEach(container => container.addEventListener('mouseover', () => { this.changeContainer(container) }));
+    },
+    changeContainer(container) {
+        NavbarConfig.removeActive();
+        let containerReceive = container.classList[1];
+       
+        switch(containerReceive){
+            
+            case "home":{
+                let aHref = document.querySelector('.navbar-link a[href="#home"]');
+                let li = aHref.parentNode;
+                NavbarConfig.changeButton(li);
+                break;
+            }
+
+            case "about":{
+                let aHref = document.querySelector('a[href="#about"]');
+                let li = aHref.parentNode;
+                NavbarConfig.changeButton(li);
+                break;
+            }
+
+            case "jobs":{
+                let aHref = document.querySelector('a[href="#jobs"]');
+                let li = aHref.parentNode;
+                NavbarConfig.changeButton(li);
+                break;
+            }
+
+            case "contato":{
+                let aHref = document.querySelector('a[href="#contact"]');
+                let li = aHref.parentNode;
+                NavbarConfig.changeButton(li);
+                break;
+            }
+
+            case "imobiliaria":{
+                let aHref = document.querySelector('a[href="#imobiliaria"]');
+                let li = aHref.parentNode;
+                NavbarConfig.changeButton(li);
+                break;
+            }
+            
+        }
+    },
+}
 
 
 
