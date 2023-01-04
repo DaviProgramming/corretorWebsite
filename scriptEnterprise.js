da5b77085b4718aab07c22ace52f34721cfbc0dc
David Oliveira <queriaprogramar@gmail.com>
2023-01-04 18:54:52 -0300

    ajustes na responsividade

---
+++ scriptEnterprise.js
@@ -0,0 +1,71 @@
+window.addEventListener('scroll', () => {
+    let navbar = document.querySelector('.navbar');
+
+    let windowPosition = window.scrollY > 0;
+
+    navbar.classList.toggle('scroll', windowPosition);
+})
+
+const startInteractive = () => {
+    let getButtonNavbar = document.querySelectorAll('.navbar-link');
+    getButtonNavbar.forEach(button => button.addEventListener('click', () => { NavbarConfig.changeButton(button) }))
+
+    let hamburguerButton = document.querySelector('.navbar-hamburguer');
+    hamburguerButton.addEventListener('click', () => { NavbarConfig.mobileOpen() })
+
+    let closeButton = document.querySelector('.navbar-close');
+    closeButton.addEventListener('click', () => { NavbarConfig.mobileClose() })
+
+}
+
+const NavbarConfig = {
+    removeActive() {
+        let activeButton = document.querySelector('.navbar-link.active');
+
+        if (activeButton != null) {
+            activeButton.classList.remove('active');
+        }
+        return;
+    },
+    changeButton(button) {
+
+        this.removeActive();
+        let buttonToActive = button;
+        buttonToActive.classList.add('active');
+
+    },
+    mobileOpen() {
+        let navbar = document.querySelector('.navbar');
+        navbar.classList.add('show');
+    },
+    mobileClose() {
+        let navbar = document.querySelector('.navbar');
+        navbar.classList.remove('show');
+    }
+}
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        autoHeight: false,
+       
+        loop: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            780: {
+                items: 1
+            },
+            1000: {
+                items: 3
+            }
+        },
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true
+
+    });
+})
+
+startInteractive();
